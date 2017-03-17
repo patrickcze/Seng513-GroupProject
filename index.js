2f06fb18108089df29b6b6062e166d3721efce4d
patrickcze <patrick.cze@gmail.com>
2017-03-16 18:44:44 -0600

    added in mongodb info

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var mongo = require('mongodb').MongoClient;
 
+/*MongoDB connnecyion info on mLab
+mongodb://<dbuser>:<dbpassword>@ds133340.mlab.com:33340/umapit
+dbuser:main
+dbpassword:seng513
+*/
+
+mongo.connect('mongodb://main:seng513@ds133340.mlab.com:33340/umapit', (err, db) => {
+
+});
 
 http.listen(port, function () {
     console.log('listening on port', port);
