0f22168e04721e6f5daed2561d7b96be3baf7bb5
Sasha Ivanov <bamboostickk@gmail.com>
2017-04-16 20:06:05 -0600

    update from develop branch

---
+++ app/datasetParser.js
@@ -0,0 +1,23 @@
+module.exports = {
+    uploadJSONtoFirebase: function (userID, datasetname, jsonData) {
+        //setup connecyion to fireabse
+        var firebase = require('firebase');
+
+        //Get a new static firebase reference key
+        let newDatasetKey = firebase.database().ref('datasets').push().key;
+
+        let datasetDetails = {
+            name: datasetname,
+            userID: userID
+        };
+
+        //Upload the users data to firebase
+        var updates = {};
+        updates['/datasets/' + newDatasetKey + '/data'] = jsonData;
+        updates['/datasets-metadata/'+newDatasetKey] = datasetDetails;
+        updates['/users/' + userID + '/datasets/' + newDatasetKey] = true;
+
+        return firebase.database().ref().update(updates);
+    }
+};
+
