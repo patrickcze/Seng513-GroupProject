9c852ac39f382d80070e2bf9cb3310fa40100067
Sasha Ivanov <bamboostickk@gmail.com>
2017-04-16 19:37:28 -0600

    Share map has dark theme
    dark theme , share map

--- public/js/project.js
+++ public/js/project.js
@@ -277,7 +277,7 @@ function setupViewOnlyProject(id, socket) {
         map.zoomControl.setPosition('bottomright');
 
         
-        L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+        L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
         }).addTo(map);
 
